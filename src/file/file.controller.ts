5085d326f1f2a7e98391d0b7f288095edf92c0ea
Shashi Sharma <sharmashashi940@gmail.com>
2023-06-11 22:10:41 +0545

    add endpoint to get files with different id types

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,9 +1,13 @@
 import {
+  Body,
   Controller,
   Delete,
+  Get,
   Param,
   ParseIntPipe,
   Post,
+  Query,
+  Req,
   Request,
   UploadedFile,
   UseGuards,
@@ -13,6 +17,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { multerConfig } from 'src/config/multer.config';
 import { FileService } from './file.service';
+import { FileDto } from './dto/file.dto';
 
 @Controller('file')
 export class FileController {
@@ -20,13 +25,40 @@ export class FileController {
   @UseGuards(JwtGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', multerConfig))
-  async upload(@UploadedFile() file: Express.Multer.File, @Request() req) {
-    return this.fileService.saveFile(file, req.user.id);
+  async upload(
+    @UploadedFile() file: Express.Multer.File,
+    @Request() req,
+    @Body() body: any,
+  ) {
+    const dto = new FileDto();
+    dto.folderId = parseInt(body.folderId);
+
+    dto.userId = req.user.id;
+    return this.fileService.saveFile(file, dto);
   }
 
   @UseGuards(JwtGuard)
   @Delete('delete/:id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return this.fileService.deleteFile(id);
+    return await this.fileService.deleteFile(id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get()
+  async getFileById(@Query('id', ParseIntPipe) id: number) {
+    return await this.fileService.getFileById(id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get()
+  async getFilesByFolderId(@Query('folderId', ParseIntPipe) folderId: number) {
+    return await this.fileService.getFilesByFolderId(folderId);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('/me')
+  async getFilesByUserId(@Request() req) {
+    console.log(req);
+    return await this.fileService.getFilesByUserId(req.user.id);
   }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,25 +1,37 @@
-import { Injectable, NotFoundException, UploadedFile } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { FileDto } from './dto/file.dto';
 
 @Injectable()
 export class FileService {
   constructor(private prisma: PrismaService) {}
-  async saveFile(@UploadedFile() file: Express.Multer.File, userId: number) {
-    const user = await this.prisma.user.findUnique({ where: { id: userId } });
-    if (!user) throw new NotFoundException();
-    const savedFile = await this.prisma.file.create({
-      data: {
-        folder_id: null,
-        filename: file.originalname,
-        path: file.path,
-        mimetype: file.mimetype,
-        size: file.size,
-        owner_id: userId,
-      },
-    });
-    delete savedFile.owner_id;
-    return savedFile;
+  async saveFile(file: Express.Multer.File, dto: FileDto) {
+    try {
+      const savedFile = dto.folderId
+        ? await this.prisma.file.create({
+            data: {
+              filename: file.originalname,
+              path: file.path,
+              mimetype: file.mimetype,
+              size: file.size,
+              folder: { connect: { id: dto.folderId } },
+              owner: { connect: { id: dto.userId } },
+            },
+          })
+        : await this.prisma.file.create({
+            data: {
+              filename: file.originalname,
+              path: file.path,
+              mimetype: file.mimetype,
+              size: file.size,
+              owner: { connect: { id: dto.userId } },
+            },
+          });
+      return savedFile;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async deleteFile(id: number) {
@@ -33,6 +45,42 @@ export class FileService {
         }
       }
       return 'Something went wrong!';
+    }
+  }
+
+  async getFileById(id: number) {
+    try {
+      const file = await this.prisma.file.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      if (!file) throw new NotFoundException('File not found!');
+      return file;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getFilesByFolderId(id: number) {
+    try {
+      const files = await this.prisma.file.findMany({
+        where: { folder_id: id },
+      });
+      return files;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getFilesByUserId(id: number) {
+    try {
+      const files = await this.prisma.file.findMany({
+        where: { owner_id: id },
+      });
+      return files;
+    } catch (error) {
+      throw error;
     }
   }
 }
